920bea008903414b07a9f03911e7394e362d80a2
哈 <41052400+ydx17634012122@users.noreply.github.com>
2021-07-06 14:53:12 +0800

    Add files via upload

---
+++ 我爱我家租房.py
@@ -0,0 +1,169 @@
+import requests
+from lxml import etree
+import pandas as pd
+import re
+from threading import Thread
+import os
+
+
+ABSDIR_PATH = os.path.abspath(os.path.dirname(__file__))
+DEFAULT_CSV_PATH = os.path.join(ABSDIR_PATH, "5i5j.csv")
+
+class Storage:
+    def __init__(self, *columns):
+        #excel 或者 csv的列名
+        self.data = []
+        self.columns = [i for i in columns]
+
+    def add(self, data=None):
+        self.data.append(data)
+
+    def storage(self, path, mode="Excel"):
+        if mode == "Excel":
+            data = pd.DataFrame(data=self.data, columns=self.columns)
+            data.to_excel(path, index_label=False, index=False, encoding='utf-8')
+            print(f"存储excel文件完成  ==> {path}")
+        elif mode == "CSV":
+            data = pd.DataFrame(data=self.data, columns=self.columns)
+            data.to_csv(path, index_label=False, index=False, encoding='utf-8')
+            print(f"存储csv文件完成  ==> {path}")
+        else:
+            pass
+
+
+class Clean:
+    """xpath_clean"""
+    @classmethod
+    def xpath(cls, html, **kwargs):
+        html_xpath = etree.HTML(html)
+        data = {}
+        for k, v in kwargs.items():
+            data[k] = html_xpath.xpath(v)
+        return data
+
+
+class Sipider5i5j:
+    def __init__(self, min_price, max_price,
+                 domain="https://bj.5i5j.com/",
+                 storagePath=DEFAULT_CSV_PATH,
+                 storageMode='CSV',
+                 max_list_page=5000):
+        """
+        min_price: 最低价格,
+        max_price: 最高价格
+        storagePath: 保存路径
+        storageMode, 保存方式 两种 Excel 或者 CSV
+        """
+        self.domain = domain if domain[-1] != '/' else domain[:-1]
+        self.max_list_page = max_list_page
+        self.start_url = f"{self.domain}/zufang/b{min_price}e{max_price}o3r1r2r3r10u1u2"
+        self.storage = Storage(
+            "title", "房源ID", "价格", "支付方式", "户型", "楼层", "面积", "朝向", "小区", "楼型", "电梯",
+            "供暖", "出租方式", "看房时间", "区域", "户型结构", "中介费", "腾空时间", "地铁", "服务费", "配套设施",
+            "亮点", "户型介绍", "交通", "周边设施", "url", "租售"
+        )
+        self.storagePath = storagePath
+        self.storageMode = storageMode
+        self.headers = {
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36",
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                    "Cookie": "HMF_CI=3a3e735027edec65dfba67ffd0e1f8345b8c040262c09ca5bc2395f86d2f24317f; _ga=GA1.2.2056239572.1623115085; _dx_uzZo5y=c53cf618bf60d72c44f93e412efd6c98bdf7079edbd75c1124964b3b30c7086673c5a656; smidV2=202106080918052c70d505f1e2f42032b625b11d1800f500c740419b8d02580; gr_user_id=61309069-623d-425b-b3cd-9e6d3e519f32; yfx_c_g_u_id_10000001=_ck21060809181512179838583229918; __TD_deviceId=KL931NIAER3OBL4V; __tea_cookie_tokens_221025=%257B%2522web_id%2522%253A%25226972406977310377509%2522%252C%2522ssid%2522%253A%25227a003abf-c4ae-45ea-b5dd-07036b3cef82%2522%252C%2522user_unique_id%2522%253A%25226972406977310377509%2522%252C%2522timestamp%2522%253A1623390007188%257D; yfx_mr_n_10000001=baidu%3A%3Amarket_type_ocpc%3A%3A%3A%3Abaidu_ppc%3A%3A%3A%3A%3A%3A%25E5%258C%2597%25E4%25BA%25AC%25E6%2588%25BF%25E5%25A4%25A9%25E4%25B8%258B%25E5%259C%25B0%25E4%25BA%25A7%3A%3Awww.baidu.com%3A%3A92091967304%3A%3A%3A%3A%25E4%25BA%258C%25E6%2589%258B%25E6%2588%25BF%25E7%25AB%259E%25E5%2593%2581%25E8%25AF%258D%3A%3A%25E6%2588%25BF%25E5%25A4%25A9%25E4%25B8%258B%3A%3A36%3A%3Apmf_from_adv%3A%3Abj.5i5j.com%2Fershoufang%2F; yfx_key_10000001=; yfx_mr_f_n_10000001=baidu%3A%3Amarket_type_ocpc%3A%3A%3A%3Abaidu_ppc%3A%3A%3A%3A%3A%3A%25E5%258C%2597%25E4%25BA%25AC%25E6%2588%25BF%25E5%25A4%25A9%25E4%25B8%258B%25E5%259C%25B0%25E4%25BA%25A7%3A%3Awww.baidu.com%3A%3A92091967304%3A%3A%3A%3A%25E4%25BA%258C%25E6%2589%258B%25E6%2588%25BF%25E7%25AB%259E%25E5%2593%2581%25E8%25AF%258D%3A%3A%25E6%2588%25BF%25E5%25A4%25A9%25E4%25B8%258B%3A%3A36%3A%3Apmf_from_adv%3A%3Abj.5i5j.com%2Fershoufang%2F; PHPSESSID=u7hii9br1bld3cv24kkjj40lpq; domain=bj; historyCity=%5B%22%5Cu5317%5Cu4eac%22%5D; pc_pmf_group_bj=c06dd55fcad35682b98e9e44bbdfde4e403fe416c1ee50a643fdd58c7d9543b6a%3A2%3A%7Bi%3A0%3Bs%3A15%3A%22pc_pmf_group_bj%22%3Bi%3A1%3Bs%3A154%3A%22%7B%22pmf_group%22%3A%22baidu%22%2C%22pmf_medium%22%3A%22ppzq%22%2C%22pmf_plan%22%3A%22%5Cu5de6%5Cu4fa7%5Cu6807%5Cu9898%22%2C%22pmf_unit%22%3A%22%5Cu6807%5Cu9898%22%2C%22pmf_keyword%22%3A%22%5Cu6807%5Cu9898%22%2C%22pmf_account%22%3A%22160%22%7D%22%3B%7D; baidu_OCPC_pc=7ec231ff9b700749887322edaff1a007813ca3668b791e02e38ce503395aa14ea%3A2%3A%7Bi%3A0%3Bs%3A13%3A%22baidu_OCPC_pc%22%3Bi%3A1%3Bs%3A178%3A%22%22http%3A%5C%2F%5C%2Fbjh.5i5j.com%5C%2F%3Fpmf_group%3Dbaidu%26pmf_medium%3Dppzq%26pmf_plan%3D%25E5%25B7%25A6%25E4%25BE%25A7%25E6%25A0%2587%25E9%25A2%2598%26pmf_unit%3D%25E6%25A0%2587%25E9%25A2%2598%26pmf_keyword%3D%25E6%25A0%2587%25E9%25A2%2598%26pmf_account%3D160%22%22%3B%7D; _gid=GA1.2.1299598005.1625016868; 8fcfcf2bd7c58141_gr_session_id=1391d417-c42b-4252-9622-cbb764abe421; 8fcfcf2bd7c58141_gr_session_id_1391d417-c42b-4252-9622-cbb764abe421=true; _Jo0OQK=75160DEF080430262C18FC3C431600FF3642224520260E07059DA9BEBCA74EB49E2783DFC147A089CC22F45361BC9D5ED0E6ED6B65EA93B9D40501F284C1168CC6B26107A0B847CA53375B268E06EC955BB75B268E06EC955BB9D992FB153179892GJ1Z1ZQ==; Hm_lvt_94ed3d23572054a86ed341d64b267ec6=1624408592,1624519846,1625016868,1625017045; zufang_BROWSES=90124948%2C90045919%2C90076561%2C90153637%2C90238177%2C90239949%2C90231987%2C501495142%2C90051789%2C90212408%2C90174795%2C90126850%2C90045091%2C90163454%2C501478442; yfx_f_l_v_t_10000001=f_t_1623115095206__r_t_1625017046796__v_t_1625023039310__r_c_4; _gat=1; Hm_lpvt_94ed3d23572054a86ed341d64b267ec6=1625023051",
+                    "Upgrade-Insecure-Requests": '1',
+                    "Sec-Fetch-User": '?1',
+                }
+
+    def get_html(self, url, string):
+        html = requests.get(url, headers=self.headers).text
+        wscckey = re.findall(r'<HTML><HEAD><script>window\.location\.href="(.*?)";</script></HEAD><BODY>', html)
+        if wscckey:
+            url = wscckey[0]
+            html = requests.get(url, headers=self.headers).text
+        print(f"长在抓取{string} : {url}")
+        return html
+
+    def get_detail_url(self, html, xpath):
+        data = Clean.xpath(html, **xpath)
+        data = [f"{self.domain}{i}" for i in data["detail_urls"]]
+        return data
+
+    def get_detail_data(self, html, url):
+        xpath = {
+            "title": 'string(//h1[@class="house-tit"])',
+            "房源ID": 'string(//span[@class="del-houseid"])',
+            '价格': 'string(//p[@class="de-price"]/span)',
+            '支付方式': 'string(//span[@class="yafu "])',
+            '户型': 'string(//div[@class="jlyoubai fl jlyoubai1"]//p[@class="houseinfor1"])',
+            '楼层': 'string(//div[@class="jlyoubai fl jlyoubai1"]//p[@class="houseinfor2"])',
+            "面积": 'string(//div[@class="jlyoubai fl jlyoubai2"]//div[@class="jlquannei"]//p[@class="houseinfor1"])',
+            "朝向": 'string(//div[@class="jlyoubai fl jlyoubai3"]//div[@class="jlquannei"]//p[@class="houseinfor1"])',
+            "配套设施": '//ul[@class="fysty"]//text()',
+            "亮点": 'string(//ul[@class="fytese"]/child::li[1]/label)',
+            "户型介绍": 'string(//ul[@class="fytese"]/child::li[2]/label)',
+            '交通': 'string(//ul[@class="fytese"]/child::li[3]/label)',
+            '周边设施': 'string(//ul[@class="fytese"]/child::li[4]/label)',
+            "小区信息": 'string(//ul[@class="fytese"]/child::li[4]/label)',
+            "租售": '//div[@class="zushous"]//text()'
+        }
+        data = Clean.xpath(html, **xpath)
+        data['租售'] = re.sub(r'\s+', "\n", '\n'.join(data['租售'])).replace("无", "无\n").replace('：\n', ': ')
+        data["url"] = url
+        data2 = data['租售'].split('\n')
+        for i in data2:
+            if ":" in i:
+                k, v = i.split(":")
+                data[k] = v
+        data['户型'] = re.sub('\s', '', data["户型"])
+        data['支付方式'] = data['支付方式'].replace("(支付方式：", '').replace(")", '')
+        data['房源ID'] = data["房源ID"].replace("房源ID：", "")
+        data['配套设施'] = re.sub(r'\s+', '',",".join(data["配套设施"]))[1:-1]
+        d = {}
+        for i in self.storage.columns:
+            d[i] = data.get(i, 'None')
+        return d
+
+    def clean_detail(self, detail_url):
+        detail_html = self.get_html(detail_url, "房源信息页")
+        house_data = self.get_detail_data(detail_html, url=detail_url)
+        self.storage.add(house_data)
+
+    def run(self):
+        i = 1
+        while True:
+            print(f"正在抓取第{i}页")
+            list_url = f'{self.start_url}n{i}/'
+            list_html = self.get_html(list_url, "列表页")
+            detail_urls = self.get_detail_url(list_html, {"detail_urls": '//h3[@class="listTit"]/a/@href'})
+            if not detail_urls or (i > self.max_list_page):
+                break
+            ts = []
+            for detail_url in detail_urls:
+                ts.append(Thread(target=self.clean_detail, args=(detail_url, )))
+            [t.start() for t in ts]
+            [t.join() for t in ts]
+            i += 1
+        self.storage.storage(self.storagePath, mode=self.storageMode)
+
+
+def start_spider(spider_setting):
+    #最低价格  最高价格  保存路径(默认为当前执行路径)  保存文件格式 (Excel 与 csv 默认 csv)  换省份改一下domain就行
+    Sipider5i5j(
+        min_price=spider_setting["最低价"],
+        max_price=spider_setting["最高价"],
+        domain=spider_setting["域名"],
+        storagePath=spider_setting["存储路径"],
+        storageMode=spider_setting["保存文件格式"],
+        max_list_page=spider_setting["抓取页码数量"]
+    ).run()
+
+
+if __name__ == "__main__":
+    #最低价格  最高价格  保存路径(默认为当前执行路径)  保存文件格式 (Excel 与 csv 默认 csv)  换省份改一下domain就行
+    spider_setting = {
+        "最低价": 500,
+        "最高价": 10000,
+        "域名": "https://bj.5i5j.com/",
+        "存储路径": r'C:\Users\xq8\Desktop\新建文件夹 (2)\5i5j.csv',
+        "保存文件格式": "CSV",  #或者csv  csv后缀为.csv
+        "抓取页码数量": 1000
+    }
+    start_spider(spider_setting)
